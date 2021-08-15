5468b1f36219b46035d8cb1ca34070e4837da3c8
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-08-15 17:07:52 +0700

    can create item through web

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,13 @@ class MoviesController < ApplicationController
   def new
     # default: render 'new' template
   end
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :release_date)
+    end
 end
