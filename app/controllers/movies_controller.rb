42bf00ee32c9becb47702829d2bfbf69ca835ff3
Sathaporn-Phila <nuinee_2544@hotmail.com>
2021-08-15 19:16:22 +0700

    patch 1.5 can edit && delete item

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,15 +3,36 @@ class MoviesController < ApplicationController
     @movie = Movie.find_by_id(params[:id]) # what if this movie not in DB?
     # BUG: we should check @movie for validity here!
   end
+
   def new
     # default: render 'new' template
   end
+
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+   
+  def update
+      @movie = Movie.find params[:id]
+      @movie.update(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
   private
+
     def movie_params
       params.require(:movie).permit(:title, :rating, :release_date)
     end
